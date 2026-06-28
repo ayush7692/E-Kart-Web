6f153bdb9fde0b115b9cd278593b32e47302fa29
ayush7692 <you@example.com>
2026-06-28 12:32:16 +0530

    c

--- Server-Side/src/controller/userController.js
+++ Server-Side/src/controller/userController.js
@@ -88,11 +88,7 @@ const getAddress = async(req,res)=>{
         res.status(404)
         throw new Error('No Address found')
     }
-    if(myAddress.length===0){
-        res.status(404)
-        throw new Error('first add Address')
-    }
-
+ 
     res.status(200).json(myAddress)
 
 }
